c6b978c376c1ddf91cd1786f3dd419446353f2b4
milalaila <rahmadhanilaila8@gmail.com>
2025-10-16 13:50:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Anda bisa menambahkan JavaScript di sini jika diperlukan
+// Contoh: animasi saat scroll, interaksi tambahan, dll.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('CV Website loaded');
+});
